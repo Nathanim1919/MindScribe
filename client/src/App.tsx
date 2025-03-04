62f43906109379fc4a21756b568d05e9784d3620
nathanim1919 <nathanim2tadele@gmail.com>
2025-03-05 01:40:58 +0300

    initialize client and server structure with basic configurations and setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <h1>
+      HELLO, VITE + REACT + TAILWINDCSS!
+    </h1>
+   </div>
+  )
+}
+
+export default App
---
+++ server/src/server.ts
@@ -0,0 +1,9 @@
+// Entry Point for the server.
+import express from 'express';
+
+const app = express();
+
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000');
+})
