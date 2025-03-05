410ae7ef02005a43711d9ad690f2b455dc77a334
nathanim1919 <nathanim2tadele@gmail.com>
2025-03-05 17:31:34 +0300

    add Prettier configuration and enhance ESLint setup; update styles and structure in client

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    settings: {
+      react: {
+        version: 'detect', // Ensures React version is detected automatically
+      },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/react-in-jsx-scope': 'off', // Disable the rule that requires React in scope
     },
   },
-)
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'], // Ensures compatibility with React 17+
+];
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-   <div>
-    <h1>
-      HELLO, VITE + REACT + TAILWINDCSS!
-    </h1>
-   </div>
-  )
+    <div className='text-ava'>
+      <h1>HELLO, VITE + REACT + TAILWINDCSS!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
