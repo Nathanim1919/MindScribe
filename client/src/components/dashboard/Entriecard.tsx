7522cc45485e0909d46cc6001029df16dbfdcd01
nathanim1919 <nathanim2tadele@gmail.com>
2025-03-09 22:07:53 +0300

    style: enhance dark mode styling for Entriecard component to improve visual consistency

--- client/src/components/dashboard/Entriecard.tsx
+++ client/src/components/dashboard/Entriecard.tsx
@@ -39,15 +39,15 @@ export const Entriecard: React.FC<EntriecardPropType> = ({ entries }) => {
           {mood}
         </span>
         <button onClick={likeEntry} aria-label="Like">
-          <IoIosHeartEmpty className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-dark-700" />
+          <IoIosHeartEmpty className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-dark-400" />
         </button>
       </div>
-      <div className="px-4">
+      <div className="px-4 grid gap-1">
         <h3 className="text-1xl font-bold dark:text-dark-700">{title}</h3>
-        <p className="text-[14px] text-dark-400 w-full">{content}</p>
+        <p className="text-[14px] text-light-600 dark:text-dark-500 w-full">{content}</p>
       </div>
       <div className="flex px-4 bg-gray-100 dark:bg-dark-100 py-2 justify-between items-center border-t border-gray-200 dark:border-dark-100">
-        <span className="text-[13px] w-full text-gray-500 flex items-center gap-1">
+        <span className="text-[13px] w-full text-light-600 flex items-center gap-1">
           <CiCalendarDate />
           {date}
         </span>
@@ -57,14 +57,14 @@ export const Entriecard: React.FC<EntriecardPropType> = ({ entries }) => {
             aria-label="Edit"
             className="rounded-full border-dark-50"
           >
-            <CiEdit className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-white" />
+            <CiEdit className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-dark-400" />
           </button>
           <button
             onClick={deleteEntry}
             aria-label="Delete"
             className="rounded-full border-dark-50"
           >
-            <MdDeleteOutline className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-white" />
+            <MdDeleteOutline className="icon dark:bg-dark-100 hover:dark:bg-dark-200 dark:text-dark-400" />
           </button>
         </div>
       </div>
